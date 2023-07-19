f0f67506eedb46876095e8514b73b6f2c1d215ad
jonimarqu <marquez.jonas91@gmail.com>
2023-07-18 22:07:33 -0300

    Resuelto aplicando el opcional de Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
 
+type Product = {
+  name: string;
+  price: string;
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'entrega';
+
+  products: Product[] = [
+    {
+      name: 'Notebook',
+      price: '1000',
+    },
+    {
+      name: 'Tablet',
+      price: '500',
+    },
+    {
+      name: 'Smartphone',
+      price: '300',
+    },
+    {
+      name: 'Television',
+      price: '2000',
+    },
+  ];
+
+  isDisplayed = true;
+
+  toggleDisplay() {
+    if (this.isDisplayed) {
+      this.isDisplayed = false;
+    } else {
+      this.isDisplayed = true;
+    }
+  };
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, MatButtonModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
